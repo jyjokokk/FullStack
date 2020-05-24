a520fe6de07772b883321647d27b2eb83525ff8c
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-05-24 06:58:59 +0300

    Part 1 - Step 1 completed

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -1,6 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+/**
+ * Header component
+ * @component
+ * @param {string} props to feed to for component
+ */
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+
+/**
+ * Container for the main body of page
+ * @param {string} props  to feed for the component
+ */
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  )
+}
+
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
+  )
+}
+
+
+/**
+ * Main component to run
+ */
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,19 +60,23 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content 
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   )
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
