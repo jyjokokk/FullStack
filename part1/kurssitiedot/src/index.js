03aecb6a13ffaedbd0f9ce0f1facabea5776bd6b
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-07-02 00:45:27 +0300

    Refactored index.js

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -3,14 +3,9 @@
  * @author Jyrki Kokkola
  * @todo Unsure about the JSDoc format
  */
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
 
 /**
  * Header component
@@ -21,16 +16,10 @@ const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
-<<<<<<< HEAD
     </div>
   );
 };
-=======
-    </>
-  );
-};
 
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
 
 /**
  * Component for the main body of page
@@ -42,16 +31,9 @@ const Content = (props) => {
       <Part part={props.part1} exercises={props.exercises1} />
       <Part part={props.part2} exercises={props.exercises2} />
       <Part part={props.part3} exercises={props.exercises3} />
-<<<<<<< HEAD
     </div>
   );
 };
-=======
-    </>
-  );
-};
-
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
 
 /**
  * Component for part + exercises
@@ -59,7 +41,6 @@ const Content = (props) => {
  */
 const Part = (props) => {
   return (
-<<<<<<< HEAD
     <div>
       <p>
         {props.part} {props.exercises}
@@ -67,14 +48,6 @@ const Part = (props) => {
     </div>
   );
 };
-=======
-    <>
-      <p>{props.part} {props.exercises}</p>
-    </>
-  );
-};
-
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
 
 /**
  * Component for the total sum of exercises
@@ -82,7 +55,6 @@ const Part = (props) => {
  */
 const Total = (props) => {
   return (
-<<<<<<< HEAD
     <div>
       <p>
         Number of exercises{" "}
@@ -91,34 +63,17 @@ const Total = (props) => {
     </div>
   );
 };
-=======
-    <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
-  );
-};
-
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
 
 /**
  * Main component to run
  */
 const App = () => {
-<<<<<<< HEAD
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
   const part2 = "Usings props to pass data";
   const exercises2 = 7;
   const part3 = "State of a component";
-=======
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Usings props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
   const exercises3 = 14;
 
   return (
@@ -140,12 +95,5 @@ const App = () => {
     </div>
   );
 };
-<<<<<<< HEAD
-
-ReactDOM.render(<App />, document.getElementById("root"));
-=======
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
->>>>>>> 643b87bf6e99d31b704cd6e10975fe860fc2dd42
