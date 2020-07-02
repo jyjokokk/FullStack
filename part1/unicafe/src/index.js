9d63abb5fc56a9eca5394937228705a451005317
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-07-02 03:47:23 +0300

    Unicafe - Step 1 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+/**
+ * Web application for giving feedback to Unicafe.
+ * (Exercises 1.6 - 1.14 of Fullstack Open mooc)
+ * @author Jyrki Kokkola
+ * @version 0.0.1
+ */
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Give Feedback</h2>
+        <Button onClick={handleGoodClick} text='Good' />
+        <Button onClick={handleNeutralClick} text='Neutral' />
+        <Button onClick={handleBadClick} text='Bad' />
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
