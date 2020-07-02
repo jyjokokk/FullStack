9bc38028715357a6da1600a0cc073788d236b66a
jyjokokk <jyrki.j.kokkola@student.jyu.fi>
2020-07-02 04:28:27 +0300

    Unicafe - Step 2 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,34 +13,75 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
+const Average = ({ all }) => {
+  if (all.length === 0) {
+    return (
+      <div>
+        Average will be calculated once there is given feedback.
+      </div>
+    )
+  }
+  let sum = 0;
+  all.forEach(i => sum += i)
+  let avg = sum / all.length
+  return (
+    <div>
+      Average {avg}
+    </div>
+  )
+}
+
+const Percentage = ({ good, all }) => {
+  if (all.length === 0) {
+    return (
+      <div>
+      </div>
+    )
+  }
+  let percent = good / all.length * 100
+  return (
+    <div>
+      Positive {percent} %
+    </div>
+  )
+
+}
+
 const App = (props) => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
 
   const handleGoodClick = () => {
+    setAll(all.concat(1))
     setGood(good + 1)
   }
   const handleNeutralClick = () => {
+    setAll(all.concat(0))
     setNeutral(neutral + 1)
   }
   const handleBadClick = () => {
+    setAll(all.concat(-1))
     setBad(bad + 1)
   }
-
+  
   return (
     <div>
+      <h2>Give Feedback</h2>
       <div>
-        <h2>Give Feedback</h2>
         <Button onClick={handleGoodClick} text='Good' />
         <Button onClick={handleNeutralClick} text='Neutral' />
         <Button onClick={handleBadClick} text='Bad' />
       </div>
+      <h2>Statistics</h2>
       <div>
-        <h2>Statistics</h2>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
+        <div>Good {good}</div>
+        <div>Neutral {neutral}</div>
+        <div>Bad {bad}</div>
+        <div>All {all.length}</div>
+        <Average all={all} />
+        <Percentage good={good} all={all} />
       </div>
     </div>
   )
