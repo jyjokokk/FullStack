c214f2276c97c105dde8cfe77025669a7f023361
jyjokokk <jyjokokk@student.jyu.fi>
2020-11-13 05:36:26 +0200

    Tehtava 2.6 (Debug)

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Jyrki Kokkola' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat( {name: newName }))
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+                  value={newName}
+                  onChange={handleNameChange}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>
+        debug: {newName}
+      </div>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) =>
+          <p key={persons.indexOf(person)}>{person.name}</p>
+        )}
+      </div>
+    </div>
+  )
+
+}
+
+export default App;
